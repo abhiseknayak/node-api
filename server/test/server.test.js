da877bd2f39c2fcde934b9b28e6feebba3abea1d
abhisek <abknayak26@gmail.com>
2018-07-09 19:38:47 +0530

    GET route testing

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,10 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos =[{
+  text:"First todo"
+},{
+  text:"second todo"
+}];
+
 beforeEach((done)=>{
   Todo.remove({}).then(()=>{
-    done()
-  })
+    Todo.insertMany(todos);
+  }).then(()=>done());
 });
 
 describe('POST /todos',()=>{
@@ -26,8 +32,8 @@ describe('POST /todos',()=>{
         }
 
           Todo.find().then((todos)=>{
-          expect(todos.length).toBe(1);
-          expect(todos[0].text).toBe(text);
+          expect(todos.length).toBe(3);
+          expect(todos[2].text).toBe(text);
           done();
 
         }).catch((e)=>done(e));
@@ -45,9 +51,24 @@ describe('POST /todos',()=>{
       }
 
       Todo.find().then((todos)=>{
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e)=>done(e));
     })
+  });
+});
+
+
+describe("GET /todos",()=>{
+  it("should get all todos",(done)=>{
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todos.length).toBe(2);
+
+
+      })
+      .end(done);
   });
 });
