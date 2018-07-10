55ab11a68329bad2bc77f561fad00f044548ff11
abhisek <abknayak26@gmail.com>
2018-07-10 23:04:10 +0530

    added new testcases for get method

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,12 +1,14 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos =[{
+  _id:new ObjectID(),
   text:"First todo"
 },{
+  _id:new ObjectID(),
   text:"second todo"
 }];
 
@@ -69,6 +71,33 @@ describe("GET /todos",()=>{
 
 
       })
+      .end(done);
+  });
+});
+
+describe("GET /todos/:id",()=>{
+  it("should return doc",(done)=>{
+    request(app)
+      .get(`/todos/${String(todos[0]._id)}`)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+
+  });
+
+  it("should return 404 if todo not found",(done)=>{
+      request(app)
+        .get(`/todos/${(new ObjectID()).toHexString()}`)
+        .expect(404)
+        .end(done);
+  });
+
+  it("should return 404 for non object-id",(done)=>{
+    request(app)
+      .get('/todos/123')
+      .expect(404)
       .end(done);
   });
 });
